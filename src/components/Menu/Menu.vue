2a75a7d141e9712acba7907f21dbd7b85430a75c
YuliyaMikhaleva <umihaleva93@mail.ru>
2022-01-11 16:26:02 +0500

    Добавлены блоки header и menu, подключены шрифты

---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav class="menu">
+    <a href="/">
+      <Logo class="menu__logo"/>
+    </a>
+    <ul class="menu__list">
+      <div class="menu__wrp">
+        <li class="menu__item">
+          <a href="#">
+            <img src="images/homeIcon.svg" alt="home">
+          </a>
+        </li>
+        <li class="menu__item">
+          <a href="#">
+            <img src="images/petsIcon.svg" alt="pets">
+          </a>
+        </li>
+        <li class="menu__item">
+          <a href="#">
+            <img src="images/blogIcon.svg" alt="blog">
+          </a>
+        </li>
+        <li class="menu__item">
+          <a href="#">
+            <img src="images/chatIcon.svg" alt="chat">
+          </a>
+        </li>
+        <li class="menu__item">
+          <a href="#">
+            <img src="images/personIcon.svg" alt="person">
+          </a>
+        </li>
+      </div>
+      <li class="menu__item">
+        <a href="#">
+          <img src="images/logoutIcon.svg" alt="logout">
+        </a>
+      </li>
+    </ul>
+
+  </nav>
+</template>
+
+<script>
+import Logo from "@/components/Logo/Logo";
+export default {
+  name: "Menu",
+  components: {Logo}
+}
+</script>
+
+<style lang="scss" scoped>
+@import "Menu.module.scss";
+</style>
---
+++ src/components/Theme-checkbox/Theme-checkbox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="checkbox">
+    <img class="checkbox__darkTheme" src="images/darkTheme.svg" alt="darkTheme">
+    <img class="checkbox__lightTheme" src="images/lightTheme.svg" alt="darkTheme">
+    <input class="checkbox__input" type="checkbox" id="switch" />
+    <label class="checkbox__label" for="switch">Toggle</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Theme-checkbox"
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "Themecheckbox.module";
+</style>
