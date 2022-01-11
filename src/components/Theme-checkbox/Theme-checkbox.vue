2e6e92c406ae1c4d277053d5faab01ff0f0923e7
YuliyaMikhaleva <umihaleva93@mail.ru>
2022-01-12 00:02:24 +0500

    Добавлены настройки смены тем оформления (требуется настроить цвета по макету)

--- src/components/Theme-checkbox/Theme-checkbox.vue
+++ src/components/Theme-checkbox/Theme-checkbox.vue
@@ -2,14 +2,36 @@
   <div class="checkbox">
     <img class="checkbox__darkTheme" src="images/darkTheme.svg" alt="darkTheme">
     <img class="checkbox__lightTheme" src="images/lightTheme.svg" alt="darkTheme">
-    <input class="checkbox__input" type="checkbox" id="switch" />
+    <input class="checkbox__input" type="checkbox" id="switch" v-model="darkMode"/>
     <label class="checkbox__label" for="switch">Toggle</label>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Theme-checkbox"
+  name: "Theme-checkbox",
+  data() {
+    return {
+      darkMode: false,
+    }
+  },
+  mounted() {
+    let bodyElement = document.body;
+    bodyElement.classList.add("app-background");
+  },
+  watch: {
+    darkMode: function () {
+      let htmlElement = document.documentElement;
+
+      if (!this.darkMode) {
+        localStorage.setItem("theme", 'dark');
+        htmlElement.setAttribute('theme', 'dark');
+      } else {
+        localStorage.setItem("theme", 'light');
+        htmlElement.setAttribute('theme', 'light');
+      }
+    }
+  }
 }
 </script>
 
