18a1e2598c61231a1333a8968ef249822900a7d4
YuliyaMikhaleva <umihaleva93@mail.ru>
2022-01-11 18:10:08 +0500

    Добавлены 2 карточки в основной части

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app" class="main">
     <Menu class="main__menu"/>
     <Header class="main__header"/>
+    <UserData class="main__userData"></UserData>
   </div>
 </template>
 
 <script>
 import Header from './components/Header/Header.vue'
 import Menu from "@/components/Menu/Menu";
+import UserData from "@/components/User-data/User-data";
 
 export default {
   name: 'App',
   components: {
+    UserData,
     Menu,
     Header
   }
---
+++ src/components/User-data/User-data.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="user-data">
+    <article class="user-data__hello article">
+      <h1 class="user-data__title">С возвращением, Владимир</h1>
+      <p class="user-data__text">Не забудь покормить своего питомца</p>
+      <p class="user-data__text">Хорошего дня!</p>
+      <img class="user-data__picture" src="images/picture-bg.png" alt="photo">
+    </article>
+    <article class="article user-data__progress">
+      <div class="user-data__block-diagram">
+        <img src="images/diagram.png" alt="diagram">
+      </div>
+      <div class="user-data__diagram-text">
+        <h2 class="user-data__diagram-title">Ежедневный прогресс</h2>
+        <ul class="user-data__diagram-list">
+          <li class="user-data__diagram-item">Прогулка в парке</li>
+          <li class="user-data__diagram-item">Обед</li>
+          <li class="user-data__diagram-item">Перекус 1/2</li>
+          <li class="user-data__diagram-item">Игры</li>
+        </ul>
+
+      </div>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "UserData"
+}
+</script>
+
+<style scoped lang="scss">
+@import "Userdata.module";
+</style>
