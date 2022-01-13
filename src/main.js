fb5b4ff50b0caa982f7e7037fc48d895d4afe633
YuliyaMikhaleva <umihaleva93@mail.ru>
2022-01-13 18:12:24 +0500

    Работа над диаграммой

--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/style.scss'
+import Donut from 'vue-css-donut-chart';
+import 'vue-css-donut-chart/dist/vcdonut.css';
 
+import VueApexCharts from 'vue-apexcharts'
+Vue.use(VueApexCharts)
+Vue.component('apexchart', VueApexCharts)
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Donut)
+
 
 new Vue({
   router,
