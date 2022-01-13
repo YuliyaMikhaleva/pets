a693e42e617e56475273ef1136ded0fde16cde45
YuliyaMikhaleva <umihaleva93@mail.ru>
2022-01-14 01:46:14 +0500

    работа над диаграммой

--- src/components/Chart/Chart.vue
+++ src/components/Chart/Chart.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="flex-wrapper">
     <div class="single-chart">
-      <svg viewBox="0 0 36 36" class="circular-chart orange">
+      <svg viewBox="0 0 36 36" class="circular-chart red">
         <path class="circle-bg"
               d="M18 2.0845
           a 15.9155 15.9155 0 0 1 0 31.831
           a 15.9155 15.9155 0 0 1 0 -31.831"
         />
-        <!--       <path class="circle"
-                stroke-dasharray="30, 100"
-                d="M18 2.0845
-                  a 15.9155 15.9155 0 0 1 0 31.831
-                  a 15.9155 15.9155 0 0 1 0 -31.831"
-              /> -->
         <path class="circle"
-              stroke-dasharray="90,100"
+              :stroke-dasharray="number[0]+',100'"
+              d="M18 2.0845
+          a 15.9155 15.9155 0 0 1 0 31.831
+          a 15.9155 15.9155 0 0 1 0 -31.831"
+        />
+        <text x="18" y="20.35" class="percentage"></text>
+      </svg>
+    </div>
+
+    <div class="single-chart">
+      <svg viewBox="0 0 36 36" class="circular-chart yellow">
+        <path class="circle"
+              :stroke-dasharray="number[1]+',100'"
               d="M18 2.0845
           a 15.9155 15.9155 0 0 1 0 31.831
           a 15.9155 15.9155 0 0 1 0 -31.831"
@@ -25,13 +31,8 @@
 
     <div class="single-chart">
       <svg viewBox="0 0 36 36" class="circular-chart green">
-        <!--       <path class="circle-bg"
-                d="M18 2.0845
-                  a 15.9155 15.9155 0 0 1 0 31.831
-                  a 15.9155 15.9155 0 0 1 0 -31.831"
-              /> -->
         <path class="circle"
-              stroke-dasharray="60, 100"
+              :stroke-dasharray="number[2]+',100'"
               d="M18 2.0845
           a 15.9155 15.9155 0 0 1 0 31.831
           a 15.9155 15.9155 0 0 1 0 -31.831"
@@ -40,22 +41,6 @@
       </svg>
     </div>
 
-    <div class="single-chart">
-      <svg viewBox="0 0 36 36" class="circular-chart blue">
-        <!--       <path class="circle-bg"
-                d="M18 2.0845
-                  a 15.9155 15.9155 0 0 1 0 31.831
-                  a 15.9155 15.9155 0 0 1 0 -31.831"
-              /> -->
-        <path class="circle"
-              stroke-dasharray="30, 100"
-              d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-        />
-        <text x="18" y="20.35" class="percentage"></text>
-      </svg>
-    </div>
   </div>
 </template>
 
@@ -66,9 +51,50 @@ export default {
   name: 'Chart',
   data(){
     return {
-      sections: [ 90, 60, 30]
+      sections: [ 25,35,18]
     };
+  },
+  computed: {
+    number() {
+      // const newSections = this.sections.sort((a,b) => b-a);
+      // console.log(newSections,'newSections');
+      // const indexes = this.sections.map((index) => index)
+      const newArray = this.sections.map((el, index) => {
+        if (index != 0){
+          let newIndex = index-1;
+
+          return el+this.sections[newIndex]
+        } else {
+          return el
+        }
+
+      })
+      console.log('newArray', newArray)
+
+
+      // const newArr = this.sections.map( (el) => {
+      //   const array = this.sections.filter((item) => item !== el && item > el);//массив без конкретного элемента
+      //   const summArr = array.reduce((a, b) => a+b, 0);//сумма этого массива
+      //   return el + summArr
+      // })
+
+
+
+
+
+      // const summ = this.sections.reduce((a,b) => a+b, 0)//сумма всех элементов секций
+      // const newArr = this.sections.filter((el,index) => el)
+      // const newArr = this.sections.map((el, index) => {
+      //   if (this.sections.indexOf(el) !== index){
+      //     summ
+      //   }
+      //
+      // })
+      // console.log(newArray.sort((a,b) => b-a))
+      return newArray.sort((a,b) => b-a)
+    },
   }
+
 }
 
 </script>
@@ -95,8 +121,8 @@ export default {
 
 .circle-bg {
   fill: none;
-  stroke: #eee;
-  stroke-width: 3.8;
+  stroke: #334155;
+  stroke-width: 2.8;
 }
 
 .circle {
@@ -112,16 +138,16 @@ export default {
   }
 }
 
-.circular-chart.orange .circle {
-  stroke: #ff9f00;
+.circular-chart.red .circle {
+  stroke: #F16063;
+}
+
+.circular-chart.yellow .circle {
+  stroke: #FFC542;
 }
 
 .circular-chart.green .circle {
-  stroke: #4CC790;
-}
-
-.circular-chart.blue .circle {
-  stroke: #3c9ee5;
+  stroke: #66CB9F;
 }
 
 .percentage {
