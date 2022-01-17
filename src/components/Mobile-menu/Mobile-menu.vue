d099979e6bb358fbaa57177d0e2bf8ec7e2f2517
YuliyaMikhaleva <umihaleva93@mail.ru>
2022-01-18 00:41:18 +0500

    Работа над мобильным меню

--- src/components/Mobile-menu/Mobile-menu.vue
+++ src/components/Mobile-menu/Mobile-menu.vue
@@ -2,9 +2,9 @@
     <nav class="mobile-menu">
       <div class="mobile-menu__icon" @click="isActive=!isActive" :class="{'mobile-menu__active-icon': isActive}">
         <div class="mobile-menu__wrp" :class="{'open':isActive}">
-          <span class="mobile-menu__line"></span>
-          <span class="mobile-menu__line"></span>
-          <span class="mobile-menu__line"></span>
+          <span class="mobile-menu__line" :class="{'top':isActive}"></span>
+          <span class="mobile-menu__line" :class="{'middle':isActive}"></span>
+          <span class="mobile-menu__line" :class="{'bottom':isActive}"></span>
         </div>
       </div>
     </nav>
@@ -38,17 +38,28 @@ export default {
       left: 30px;
       //transition: width 0.3s, height 0.3s, top 0.3s, left 0.3s, border-radius 0.3s;
       transition: all 0.3s;
+      /*&:active{*/
+      /*    .mobile-menu__icon::before{*/
+      /*        top: 0;*/
+      /*        left: 0;*/
+      /*    }*/
+      /*}*/
       &::before{
         display: block;
+        /*background-color: red;*/
         background-color: #4C6FFF;
         content: "";
-        width: 0;
-        height: 0;
+        width: 32px;
+        height: 32px;
         //width: 32px;
         //height: 32px;
         position: fixed;
-        top: 0;
-        left: 0;
+        top: 20px;
+        left: 30px;
+        transition: all 0.3s;
+        z-index: -1;
+        border-radius: 7px;
+
       }
     }
     &__active-icon{
@@ -62,6 +73,9 @@ export default {
       &::before{
         width: 100%;
         height: 100%;
+        top: 0;
+        left: 0;
+          border-radius: 0;
       }
     }
     &__line{
@@ -70,14 +84,28 @@ export default {
       height: 2px;
       background-color: white;
       border-radius: 1px;
+      transform-origin: 4px 0px;
+      transition: all 0.3s;
+
       &:nth-child(2){
         margin: 4px 0 4px;
       }
     }
+    &__line.top{
+        transform: translate(0, 9px) rotate(-45deg);
+        transition: all 0.3s;
+    }
+    &__line.middle{
+        transform-origin: 0% 100%;
+        opacity: 0;
+        transition: all 0.3s;
+    }
+    &__line.bottom{
+        transform: translate(2px, -8px) rotate(45deg);
+        transition: all 0.3s;
+    }
     &__wrp.open{
       position: absolute;
-      top: 10px;
-      left: 10px;
       transition: all 0.3s;
     }
 
