b7dbdba32629a750a3494f1607f13210cc1db7c5
YuliyaMikhaleva <umihaleva93@mail.ru>
2022-02-04 00:36:41 +0500

    Добавлен кастомный скролл

--- src/components/Pet-card/Pet-card.vue
+++ src/components/Pet-card/Pet-card.vue
@@ -5,9 +5,10 @@
         <swiper-slide  class="pet-card__photo-wrp" v-for="picture of pictures" :key="picture">
             <img class="pet-card__photo" :src="'/'+picture" alt="photo">
         </swiper-slide>
-
+          <div class="swiper-button-next pet-card__arrow-next" slot="button-next"></div>
+          <div class="swiper-button-prev pet-card__arrow-prev" slot="button-prev"></div>
       </swiper>
-      <div class="swiper-pagination swiper-pagination-v pet-card__pagination" slot="pagination">
+      <div v-if="pictures.length>1" class="swiper-pagination swiper-pagination-v pet-card__pagination" slot="pagination">
       </div>
 
     </div>
@@ -30,7 +31,9 @@
       </div>
       <div class="pet-card__txt">
         <h3 class="pet-card__txt-title">Описание</h3>
-        <p class="pet-card__txt-paragraph">{{info.about}}</p>
+        <vue-custom-scrollbar class="scroll-area"  :settings="settings" @ps-scroll-y="scrollHanle">
+          <p class="pet-card__txt-paragraph">{{info.about}}</p>
+        </vue-custom-scrollbar>
       </div>
       <Button class="pet-card__button" :title="'Купить - '+info.price+' Р'"/>
 
@@ -40,6 +43,8 @@
 </template>
 
 <script>
+import vueCustomScrollbar from 'vue-custom-scrollbar'
+import "vue-custom-scrollbar/dist/vueScrollbar.css"
 import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
 import 'swiper/css/swiper.css'
 import Gps from "@/../public/images/gpsIcon.svg?inline";
@@ -48,7 +53,7 @@ import {mapGetters} from "vuex";
 import moment from "moment";
 export default {
   name: "Pet-card",
-  components:{Button, Gps, Swiper, SwiperSlide},
+  components:{Button, Gps, Swiper, SwiperSlide, vueCustomScrollbar},
   props:{
     name:String,
     id:Number,
@@ -63,6 +68,11 @@ export default {
           el: ".swiper-pagination",
           dynamicBullets: true,
           clickable:true,
+          watchOverflow: true
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
         },
       },
       petAge:{
@@ -71,12 +81,20 @@ export default {
         days:null
       },
       ageNow:null,
+      settings: {
+        suppressScrollY: false,
+        suppressScrollX: false,
+        wheelPropagation: false
+      }
     }
   },
   methods:{
     moment,
     age(){
       this.ageNow = moment.duration(moment().diff(this.info.age)).locale('ru').humanize()
+    },
+    scrollHanle(evt) {
+      console.log(evt)
     }
   },
   computed: {
@@ -131,5 +149,16 @@ export default {
 
 <style lang="scss" scoped>
   @import "Pet-card.module";
-
+  .scroll-area {
+    position: relative;
+    margin: auto;
+    width: 100%;
+    height: 283px;
+  }
+  .scroll-area ::v-deep{
+    .ps__thumb-y{
+      background-color: #4C6FFF;
+      width: 4px;
+    }
+  }
 </style>
