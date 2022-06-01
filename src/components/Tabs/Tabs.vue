6987b6d2b1303558069d89b0c75066427cea5889
YuliyaMikhaleva <umihaleva93@mail.ru>
2022-06-01 10:34:03 +0500

    added page Profile and tabs in page

---
+++ src/components/Tabs/Tabs.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="tabs">
+    <div class="tabs__links">
+      <ul>
+        <a v-for="tab in tabs" :href="tab.href" @click="selectTab(tab)" :key="tab" class="tabs__item">
+          <li  :class="{ 'is-active': tab.isActive }">
+            {{ tab.name }}
+          </li>
+        </a>
+      </ul>
+    </div>
+
+    <div class="tabs__details">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tabs",
+  data() {
+    return {
+      tabs: []
+    };
+  },
+  created() {
+    this.tabs = this.$children;
+  },
+  methods:{
+    selectTab(selectedTab) {
+      this.tabs.forEach(tab => {
+        tab.isActive = (tab.name == selectedTab.name);
+      });
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss" >
+@import "Tabs";
+</style>
--- src/views/Profile/Profile.vue
+++ src/views/Profile/Profile.vue
@@ -1,18 +1,33 @@
 <template>
         <div class="profile">
-                <Error/>
+          Профиль
+          <tabs>
+            <tab name="Аккаунт" :selected="true">
+              <span>What we do</span>
+            </tab>
+            <tab name="Настройки">
+              <span>How much we do it for</span>
+            </tab>
+            <tab name="Ваши питомцы">
+              <span>Why we do it</span>
+            </tab>
+          </tabs>
         </div>
 </template>
 
 <script>
+import Tabs from "@/components/Tabs/Tabs";
+import Tab from "@/components/Tabs/Tab/Tab";
 
-import Error from "../Error/Error";
 export default {
         name: "Profile",
-        components: {Error},
+        components: {Tabs, Tab},
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .profile{
+    margin-top: 156px;
+    color: white;
+  }
 </style>
