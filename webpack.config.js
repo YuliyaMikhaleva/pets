6f6839920f34876239cc39b491fe2a194348e160
YuliyaMikhaleva <umihaleva93@mail.ru>
2022-10-04 11:06:42 +0500

    change

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 module.exports = {
     module: {
-        rules: [
-            {
-                test: /\.svg$/,
-                oneOf: [
-                    {
-                        resourceQuery: /inline/,
-                        use: [
-                            'babel-loader',
-                            'vue-svg-loader',
-                        ],
-                    },
-                    {
-                        loader: 'file-loader',
-                        query: {
-                            name: 'assets/[name].[hash:8].[ext]',
-                        },
-                    },
-                ],
-            },
-
-        ],
+        // rules: [
+        //     {
+        //         test: /\.svg$/,
+        //         oneOf: [
+        //             {
+        //                 resourceQuery: /inline/,
+        //                 use: [
+        //                     'babel-loader',
+        //                     'vue-svg-loader',
+        //                 ],
+        //             },
+        //             {
+        //                 loader: 'file-loader',
+        //                 query: {
+        //                     name: 'assets/[name].[hash:8].[ext]',
+        //                 },
+        //             },
+        //         ],
+        //     },
+        //
+        // ],
         loaders: [
             {
                 test: /\.vue$/,
